bc5cf81c189b3ff0ffe7c73919a45a40c00b4aab
Quan <61860115+Dracozure@users.noreply.github.com>
2024-11-27 22:14:34 -0800

    Recreate home page using only JS

--- src/home.js
+++ src/home.js
@@ -1,21 +1,46 @@
+import ramenBowlImg from './img/ramen-bowl.jpeg';
+
 (function() {
-    const introWords = ['like home', 'WOW', 'just right', 'amazing', 'nostalgic'];
-    let introWordIndex = 0;
-    const introSpan = document.querySelector('span.intro-word');
+    const contentContainer = document.getElementById('content');
+    const homeContent = document.createElement('div');
+    const intro = document.createElement('div');
+    const h1Intro = document.createElement('h1');
+    const introSpan = document.createElement('span');
+    const ramenBowl = document.createElement('img');
 
-    introSpan.textContent = introWords[0];
-    introSpan.style.transition = '120ms ease-in-out';
+    homeContent.classList.add('home-content');
+    intro.classList.add('intro');
+    introSpan.classList.add('intro-word');
+    ramenBowl.classList.add('ramen-bowl');
 
-    const rotateWords = setInterval(() => {
+    h1Intro.textContent = 'At Matte, it always tastes... ';
+    ramenBowl.src = ramenBowlImg;
+    ramenBowl.setAttribute('alt', 'Seafood ramen bowl');
+
+    contentContainer.appendChild(homeContent);
+    homeContent.appendChild(intro);
+    homeContent.appendChild(ramenBowl);
+    intro.appendChild(h1Intro);
+    h1Intro.appendChild(introSpan);
+
+    (function setWordRotation() {
+        const introWords = ['like home', 'WOW', 'just right', 'amazing', 'nostalgic'];
+        let introWordIndex = 0;
+    
+        introSpan.textContent = introWords[0];
+        introSpan.style.transition = '120ms ease-in-out';
+    
+        setInterval(() => {
             let nextIndex = ++introWordIndex;
-
+    
             if (nextIndex === introWords.length) {
                 nextIndex = 0;
                 introWordIndex = 0;
             }
     
             changeWord(introSpan, introWords[nextIndex]);
-        }, 8000);
+        }, 7000);
+    })();
 
     const changeWord = (element, newWord, opacity = 1, wordChanged = false) => {
         const interval = setInterval(() => {
