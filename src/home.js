8053651987c230f80b4ddaba2817f2791720fdac
Quan <61860115+Dracozure@users.noreply.github.com>
2024-11-27 20:59:02 -0800

    Add text rotate animation

--- src/home.js
+++ src/home.js
@@ -6,14 +6,41 @@
     introSpan.textContent = introWords[0];
     introSpan.style.transition = '120ms ease-in-out';
 
-    introSpan.addEventListener('click', () => {
-        let nextIndex = ++introWordIndex;
+    const rotateWords = setInterval(() => {
+            let nextIndex = ++introWordIndex;
 
-        if (nextIndex === introWords.length) {
-            nextIndex = 0;
-            introWordIndex = 0;
-        }
+            if (nextIndex === introWords.length) {
+                nextIndex = 0;
+                introWordIndex = 0;
+            }
+    
+            changeWord(introSpan, introWords[nextIndex]);
+        }, 8000);
 
-        introSpan.textContent = introWords[nextIndex];
-    });
+    const changeWord = (element, newWord, opacity = 1, wordChanged = false) => {
+        const interval = setInterval(() => {
+            if (!wordChanged) {
+                if (opacity > 0) {
+                    element.style.opacity = opacity - 0.1;
+                    clearInterval(interval);
+                    changeWord(element, newWord, opacity - 0.1, false);
+                } else {
+                    clearInterval(interval);
+                    const wait = setInterval(() => {
+                        clearInterval(wait);
+                        element.textContent = newWord;
+                        changeWord(element, newWord, opacity = 0, true);
+                    }, 300);
+                }
+            } else {
+                if (opacity < 1) {
+                    element.style.opacity = opacity + 0.1;
+                    clearInterval(interval);
+                    changeWord(element, newWord, opacity + 0.1, true);
+                } else {
+                    clearInterval(interval);
+                }
+            }
+        }, 50);
+    }
 })();
