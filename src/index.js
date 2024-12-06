e0c1c54de37b859b8e84d970722595b550a7d0a4
Quan <61860115+Dracozure@users.noreply.github.com>
2024-12-06 12:21:06 -0800

    Add nav transition buttons

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ const homeButton = document.querySelector('button.home');
 const menuButton = document.querySelector('button.menu');
 const aboutButton = document.querySelector('button.about');
 const contentElement = document.getElementById('content');
+const buttonMap = { 'home':homePage, 'menu':menuPage, 'about':aboutPage }
 
 homePage();
 
-homeButton.addEventListener('click', () => {
-    contentElement.firstChild.remove();
-    homePage();
-});
+[homeButton, menuButton, aboutButton].forEach(button => {
+    button.addEventListener('click', () => {
+        if (button !== document.querySelector('button.nav-select')) {
+            const currentButtonSelected = document.querySelector('button.nav-select');
 
-menuButton.addEventListener('click', () => {
-    contentElement.firstChild.remove();
-    menuPage();
-});
+            contentElement.firstChild.remove();
+            buttonMap[button.classList[0]]();
 
-aboutButton.addEventListener('click', () => {
-    contentElement.firstChild.remove();
-    aboutPage();
+            currentButtonSelected.classList.remove('nav-select');
+            button.classList.add('nav-select');
+        }
+    });
 });
